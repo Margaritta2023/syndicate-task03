767ed8e91bea1c64c08b8cba6ccc0ce46d512a50
Margarita Mkrtchyan <margarita_mkrtchyan@epam.com>
2024-12-01 18:23:52 +0400

    initial commit

---
+++ task03/app/lambdas/hello_world/index.js
@@ -0,0 +1,22 @@
+exports.handler = async (event) => {
+    if (event.rawPath === '/hello' && event.requestContext.http.method === 'GET') {
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify({
+                statusCode: 200,
+                message: 'Hello from Lambda',
+            }),
+        };
+        
+        return response;
+    }
+
+    const response = {
+        statusCode: 400,
+        body: JSON.stringify({
+            statusCode: 400,
+            message: `Bad request syntax or unsupported method. Request path: ${event.rawPath}. HTTP method: ${event.requestContext.http.method}`,
+        }),
+    };
+    return response;
+};
